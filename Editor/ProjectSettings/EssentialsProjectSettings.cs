0f3d709bb2bc8a2aa4cee6efbd855179e201c47f
D3TONAT0R <gdv@swissonline.ch>
2023-08-19 17:21:51 +0200

    Add project settings

---
+++ Editor/ProjectSettings/EssentialsProjectSettings.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using D3T;
+using UnityEditor;
+using System.IO;
+
+namespace D3TEditor
+{
+	public class EssentialsProjectSettings : ScriptableObject
+	{
+		private static string ProjectRootPath => Directory.GetParent(Application.dataPath).ToString();
+		private static string SettingsAssetPath => Path.Combine(ProjectRootPath, "ProjectSettings", "EssentialsProjectSettings.asset");
+
+		public static EssentialsProjectSettings Instance
+		{
+			get
+			{
+				if(instance == null) InitSettings();
+				return instance;
+			}
+		}
+		private static EssentialsProjectSettings instance;
+
+		public bool removeDefaultScriptMenu = true;
+		public string defaultScriptNamespace = "MyNamespace";
+		public string[] additionalDefaultUsings = new string[0];
+
+		private static void InitSettings()
+		{
+			if(File.Exists(SettingsAssetPath))
+			{
+				instance = CreateInstance<EssentialsProjectSettings>();
+				string json = File.ReadAllText(SettingsAssetPath);
+				JsonUtility.FromJsonOverwrite(json, instance);
+			}
+			else
+			{
+				Debug.Log("Essentials Package: Creating new project settings ...");
+				instance = CreateInstance<EssentialsProjectSettings>();
+			}
+		}
+
+		public void Validate()
+		{
+			
+		}
+
+		public void SaveModifiedProperties()
+		{
+			Validate();
+			string json = JsonUtility.ToJson(this, true);
+			File.WriteAllText(SettingsAssetPath, json);
+		}
+	}
+}
---
+++ Editor/ProjectSettings/EssentialsProjectSettingsProvider.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using D3T;
+using UnityEditor;
+using System;
+
+namespace D3TEditor
+{
+	internal static class EssentialsProjectSettingsProvider
+	{
+		[SettingsProvider]
+		internal static SettingsProvider Register()
+		{
+			var provider = new SettingsProvider("Project/Essentials", SettingsScope.Project)
+			{
+				guiHandler = OnGUI,
+			};
+			provider.deactivateHandler += OnClose;
+			return provider;
+		}
+
+		private static void OnGUI(string search)
+		{
+			EditorGUIUtility.labelWidth = 250;
+			var so = new SerializedObject(EssentialsProjectSettings.Instance);
+			var prop = so.GetIterator();
+			prop.NextVisible(true);
+			so.Update();
+			while(prop.NextVisible(false))
+			{
+				EditorGUILayout.PropertyField(prop);
+			}
+			if(so.ApplyModifiedProperties())
+			{
+				EssentialsProjectSettings.Instance.SaveModifiedProperties();
+			}
+		}
+
+		private static void OnClose()
+		{
+			EssentialsProjectSettings.Instance.SaveModifiedProperties();
+		}
+	}
+}
