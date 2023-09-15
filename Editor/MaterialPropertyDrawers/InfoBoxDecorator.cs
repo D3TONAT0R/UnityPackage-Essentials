1e44a85e44ea5e4fe76b4eda0b0cd3393a7c5d2a
D3TONAT0R <gdv@swissonline.ch>
2023-09-15 16:44:36 +0200

    Add info box material property drawer

---
+++ Editor/MaterialPropertyDrawers/InfoBoxDecorator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace D3TEditor.MaterialPropertyDrawers
+{
+	public class InfoBoxDecorator : MaterialPropertyDrawer
+	{
+		private string text;
+
+		private float lastWidth = 200;
+
+		public InfoBoxDecorator(string text)
+		{
+			this.text = text;
+		}
+
+		public override float GetPropertyHeight(MaterialProperty prop, string label, MaterialEditor editor)
+		{
+			return EditorStyles.helpBox.CalcHeight(new GUIContent(text), lastWidth);
+		}
+
+		public override void OnGUI(Rect position, MaterialProperty prop, GUIContent label, MaterialEditor editor)
+		{
+			EditorGUI.HelpBox(position, text, MessageType.None);
+			lastWidth = position.width;
+		}
+	}
+}
