a54129637c96467b89738946978c90db54cd1eea
D3TONAT0R <gdv@swissonline.ch>
2023-09-27 17:46:15 +0200

    Add editor time tracking

---
+++ Editor/EditorTimeTracking/EditorTimeTracker.cs
@@ -0,0 +1,103 @@
+﻿using D3T;
+using D3T.Utility;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace D3TEditor.TimeTracking
+{
+	public static class EditorTimeTracker
+	{
+		[System.Serializable]
+		public class SamplesData
+		{
+			public List<UserTimeSample> times = new List<UserTimeSample>();
+		}
+
+		[System.Serializable]
+		public class UserTimeSample
+		{
+			public string userId;
+			public TimeSample times;
+		}
+
+		public static bool Enabled { get; set; } = true;
+
+		private static Dictionary<string, TimeSample> times = new Dictionary<string, TimeSample>();
+
+		private static double lastTime;
+
+		private static string FileName => PersistentFileUtility.GetFullPath(PersistentFileUtility.FileLocation.DataPath, "EditorTimes.json");
+
+		[InitializeOnLoadMethod]
+		private static void Init()
+		{
+			LoadFromFile();
+			EditorApplication.update += Update;
+			AssemblyReloadEvents.beforeAssemblyReload += OnDestroy;
+			EditorApplication.quitting += OnDestroy;
+		}
+
+		private static void OnDestroy()
+		{
+			WriteToFile();
+		}
+
+		private static void Update()
+		{
+			if(Enabled && lastTime != 0)
+			{
+				float delta = (float)(EditorApplication.timeSinceStartup - lastTime);
+				string user = GetUserName();
+				if(!times.ContainsKey(user))
+				{
+					times.Add(user, new TimeSample());
+				}
+				times[user].Increase(delta);
+			}
+			lastTime = EditorApplication.timeSinceStartup;
+		}
+
+		private static string GetUserName()
+		{
+			string id = CloudProjectSettings.userId;
+			if(!string.IsNullOrWhiteSpace(id)) return id;
+			else return $"({Environment.UserName})";
+		}
+
+		private static void LoadFromFile()
+		{
+			if(File.Exists(FileName))
+			{
+				try
+				{
+					string json = File.ReadAllText(FileName);
+					var data = JsonUtility.FromJson<SamplesData>(json);
+					times.Clear();
+					foreach(var d in data.times)
+					{
+						times.Add(d.userId, d.times);
+					}
+				}
+				catch(Exception e)
+				{
+					e.LogException("Failed to read tracked editor times, disabling time tracking");
+					Enabled = false;
+				}
+			}
+		}
+
+		private static void WriteToFile()
+		{
+			SamplesData data = new SamplesData();
+			foreach(var kv in times)
+			{
+				data.times.Add(new UserTimeSample() { userId = kv.Key, times = kv.Value });
+			}
+			string json = JsonUtility.ToJson(data, true);
+			File.WriteAllText(FileName, json);
+		}
+	}
+}
---
+++ Editor/EditorTimeTracking/TimeSample.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace D3TEditor.TimeTracking
+{
+	[System.Serializable]
+	public class TimeSample
+	{
+		public float editTime = 0;
+		public float unfocussedEditTime = 0;
+		public float playTime = 0;
+
+		public void Increase(float delta)
+		{
+			if(Application.isPlaying)
+			{
+				playTime += delta;
+			}
+			else
+			{
+				bool focussed = UnityEditorInternal.InternalEditorUtility.isApplicationActive;
+				if(focussed) editTime += delta;
+				else unfocussedEditTime += delta;
+			}
+		}
+	}
+}
