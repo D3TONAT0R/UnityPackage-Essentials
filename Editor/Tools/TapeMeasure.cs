999c23dacd7a3378841e653b70c445901d9d4bb1
D3TONAT0R <gdv@swissonline.ch>
2023-11-15 18:32:43 +0100

    Convert to editor tool

--- Editor/Tools/TapeMeasure.cs
+++ Editor/Tools/TapeMeasure.cs
@@ -1,81 +1,125 @@
 ﻿using D3T;
 using UnityEditor;
+using UnityEditor.EditorTools;
 using UnityEngine;
+#if UNITY_2020_1_OR_NEWER
+using ToolManager = UnityEditor.EditorTools.ToolManager;
+#else
+using ToolManager = UnityEditor.EditorTools.EditorTools;
+#endif
 
 namespace D3TEditor.Tools {
-	public class TapeMeasure : EditorWindow {
 
-		public bool enable;
+	[EditorTool("Tape Measure")]
+	public class TapeMeasure : EditorToolBase {
+
 		public Vector3 p1;
 		public Vector3 p2;
 		public Vector3 distanceVec;
 		public float distance3d;
 		public float distance2d;
 		public float angle2d;
-		public int setPoint;
+		public int setPoint = 1;
 
 		Color color1 = Color.red;
 		Color color2 = Color.blue;
 
-		[MenuItem("Window/Tape Measure")]
-		static void Init() {
-			TapeMeasure window = (TapeMeasure)GetWindow(typeof(TapeMeasure));
-			window.Show();
-		}
-		// draw lines between a chosen game object
-		// and a selection of added game objects
+		public override bool ShowToolWindow => true;
 
-		// Window has been selected
-		void OnFocus() {
-			// Remove delegate listener if it has previously
-			// been assigned.
-			SceneView.duringSceneGui -= OnSceneGUI;
-			// Add (or re-add) the delegate.
-			SceneView.duringSceneGui += OnSceneGUI;
-			Repaint();
-		}
+		public override int ToolWindowWidth => 300;
 
-		private void OnEnable() {
-			//EditorApplication.update += OnGUI;
-		}
-
-		void OnDestroy() {
-			// When the window is destroyed, remove the delegate
-			// so that it will no longer do any drawing.
-			//EditorApplication.update -= OnGUI;
-			SceneView.duringSceneGui -= OnSceneGUI;
-		}
-
-		void OnGUI() {
-			enable = EditorGUILayout.Toggle("Enable Tool", enable);
-			GUILayout.Label("Set Point: " + setPoint);
+		protected override void OnWindowGUI()
+		{
+			EditorGUIUtility.labelWidth = 120;
 			p1 = EditorGUILayout.Vector3Field("Point 1", p1);
 			p2 = EditorGUILayout.Vector3Field("Point 2", p2);
-			GUILayout.Label("Results:", EditorStyles.boldLabel);
-			if(p1 != Vector3.zero && p2 != Vector3.zero) {
+			EditorGUILayout.Space(10);
+			if(p1 != Vector3.zero && p2 != Vector3.zero)
+			{
 				distanceVec = p2 - p1;
 				distance3d = Vector3.Distance(p1, p2);
 				distance2d = Vector2.Distance(p1.XZ(), p2.XZ());
 				var vec = p2 - p1;
-				if(vec != Vector3.zero) {
+				if(vec != Vector3.zero)
+				{
 					angle2d = Quaternion.LookRotation(p2 - p1).eulerAngles.y;
-				} else {
+				}
+				else
+				{
 					angle2d = 0;
 				}
-			} else {
+			}
+			else
+			{
 				distanceVec = Vector3.zero;
 				distance3d = 0;
 				distance2d = 0;
 				angle2d = 0;
 				GUI.enabled = false;
 			}
-			EditorGUILayout.Vector3Field("Distance vector", distanceVec);
-			EditorGUILayout.FloatField("Total distance", distance3d);
-			EditorGUILayout.FloatField("Total distance (2D)", distance2d);
-			EditorGUILayout.FloatField("Angle (2D)", angle2d);
+			GUI.enabled = false;
+			EditorGUILayout.LabelField("Distance (Per Axis)", $"X {distanceVec.x:F3},    Y {distanceVec.y:F3},    Z {distanceVec.z:F3}");
+			EditorGUILayout.LabelField("Distance (2D)", distance2d.ToString("F3"));
+			EditorGUILayout.LabelField("Distance", distance3d.ToString("F3"));
+			EditorGUILayout.LabelField("Angle (2D)", angle2d.ToString("F1"));
 			GUI.enabled = true;
-			if(Event.current.type == EventType.Repaint) {
+			if(Event.current.type == EventType.Repaint)
+			{
 				SceneView.lastActiveSceneView.Repaint();
+			}
+		}
+
+		protected override void OnSceneGUI(EditorWindow window, bool enableInteraction)
+		{
+			RaycastHit hit;
+			if(Physics.Raycast(HandleUtility.GUIPointToWorldRay(Event.current.mousePosition), out hit, 100f, SceneView.lastActiveSceneView.camera.cullingMask, QueryTriggerInteraction.Ignore))
+			{
+				Handles.color = setPoint == 1 ? color1 : color2;
+				Handles.DrawWireDisc(hit.point, hit.normal, 0.1f);
+			}
+			if(p1 != Vector3.zero && setPoint == 1)
+			{
+				DrawPoint(p1, color1);
+			}
+			if(p2 != Vector3.zero && setPoint == 2)
+			{
+				DrawPoint(p2, color2);
+			}
+			if(p1 != Vector3.zero && p2 != Vector3.zero)
+			{
+				Handles.color = Color.yellow;
+				Handles.DrawDottedLine(p1, p2, 4);
+				if(setPoint == 0)
+				{
+					float scale = 0.5f;
+					Handles.matrix = Matrix4x4.Scale(Vector3.one * scale);
+					p1 = Handles.PositionHandle(p1 / scale, Quaternion.identity) * scale;
+					p2 = Handles.PositionHandle(p2 / scale, Quaternion.identity) * scale;
+					Handles.matrix = Matrix4x4.identity;
+				}
+			}
+			if(setPoint == 1)
+			{
+				p1 = hit.point;
+			}
+			else if(setPoint == 2)
+			{
+				p2 = hit.point;
+			}
+
+			if(Event.current.type == EventType.MouseDown && Event.current.button == 0 && Event.current.modifiers != EventModifiers.Shift)
+			{
+				int controlId = GUIUtility.GetControlID(FocusType.Passive);
+				GUIUtility.hotControl = controlId;
+				if(setPoint == 0)
+				{
+					p1 = Vector3.zero;
+					p2 = Vector3.zero;
+				}
+				setPoint++;
+				setPoint %= 3;
+				Event.current.Use();
+				GUIUtility.hotControl = 0;
 			}
 		}
 
@@ -87,68 +131,7 @@ namespace D3TEditor.Tools {
 			Handles.color = Color.blue;
 			Handles.DrawDottedLine(vec + Vector3.back, vec + Vector3.forward, 1);
 			Handles.color = c;
-			Handles.DrawWireCube(vec, 0.1f.ToUVector3());
-		}
-
-		void OnSceneGUI(SceneView sceneView) {
-			if(!enable) return;
-			Repaint();
-			Handles.BeginGUI();
-			Handles.color = Color.green;
-			GUI.color = Color.red;
-			GUI.Label(new Rect(16, 16, 300, 100), "Tape Measure Tool\nis active!", EditorStyles.whiteLargeLabel);
-			if(GUI.Button(new Rect(16, 64, 100, 16), "Disable")) enable = false;
-			Handles.EndGUI();
-			//if(Event.current.mousePosition.x < sceneView.position.xMin || Event.current.mousePosition.y < sceneView.position.yMin || Event.current.mousePosition.x > sceneView.position.xMax || Event.current.mousePosition.y > sceneView.position.yMax) return;
-			RaycastHit hit;
-			if(Physics.Raycast(HandleUtility.GUIPointToWorldRay(Event.current.mousePosition), out hit, 100f, sceneView.camera.cullingMask, QueryTriggerInteraction.Ignore)) {
-				Handles.color = setPoint == 1 ? color1 : color2;
-				Handles.DrawWireDisc(hit.point, hit.normal, 0.1f);
-			} else {
-				return;
-			}
-			if(p1 != Vector3.zero && setPoint == 1) {
-				DrawPoint(p1, color1);
-			}
-			if(p2 != Vector3.zero && setPoint == 2) {
-				DrawPoint(p2, color2);
-			}
-			if(p1 != Vector3.zero && p2 != Vector3.zero) {
-				Handles.color = Color.yellow;
-				Handles.DrawDottedLine(p1, p2, 4);
-				if(setPoint == 0) {
-					float scale = 0.5f;
-					Handles.matrix = Matrix4x4.Scale(Vector3.one * scale);
-					p1 = Handles.PositionHandle(p1 / scale, Quaternion.identity) * scale;
-					p2 = Handles.PositionHandle(p2 / scale, Quaternion.identity) * scale;
-					Handles.matrix = Matrix4x4.identity;
-				}
-			}
-			if(setPoint == 1) {
-				p1 = hit.point;
-			} else if(setPoint == 2) {
-				p2 = hit.point;
-			}
-			// Retrieve the control Id
-			// Start treating your event
-			if(Event.current.keyCode == KeyCode.Escape) {
-				enable = false;
-				return;
-			}
-			if(Event.current.type == EventType.MouseDown && Event.current.button == 0 && Event.current.modifiers != EventModifiers.Shift) {
-				int controlId = GUIUtility.GetControlID(FocusType.Passive);
-				GUIUtility.hotControl = controlId;
-				if(setPoint == 0) {
-					p1 = Vector3.zero;
-					p2 = Vector3.zero;
-				}
-				setPoint++;
-				setPoint %= 3;
-				Event.current.Use();
-				GUIUtility.hotControl = 0;
-			} else {
-				GUIUtility.hotControl = GUIUtility.hotControl;
-			}
+			Handles.DrawWireCube(vec, Vector3.one * 0.1f);
 		}
 	} 
 }
