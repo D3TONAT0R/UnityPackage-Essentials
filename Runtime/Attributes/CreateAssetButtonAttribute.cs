64bc7ed5ad623e31bbce958ad558bd58656f483e
D3TONAT0R <gdv@swissonline.ch>
2023-12-04 00:01:36 +0100

    Add optional path parameter

--- Runtime/Attributes/CreateAssetButtonAttribute.cs
+++ Runtime/Attributes/CreateAssetButtonAttribute.cs
@@ -8,5 +8,13 @@ namespace D3T
 	/// Add this attribute to an object field to add a button that creates a new asset for that type.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Field)]
-	public class CreateAssetButtonAttribute : PropertyAttribute { } 
+	public class CreateAssetButtonAttribute : PropertyAttribute
+	{
+		public readonly string defaultPath;
+
+		public CreateAssetButtonAttribute(string defaultPath = null)
+		{
+			this.defaultPath = defaultPath;
+		}
+	} 
 }
