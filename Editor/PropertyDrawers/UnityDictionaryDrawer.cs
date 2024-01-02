ed02ec11bd96a4d3d04677c4f8ec7fc8349d61eb
D3TONAT0R <gdv@swissonline.ch>
2024-01-02 01:15:31 +0100

    Split into submethods

--- Editor/PropertyDrawers/UnityDictionaryDrawer.cs
+++ Editor/PropertyDrawers/UnityDictionaryDrawer.cs
@@ -90,121 +90,134 @@ namespace D3TEditor.PropertyDrawers
 					var kObj = PropertyDrawerUtility.GetTargetObjectOfProperty(k);
 					var v = values.GetArrayElementAtIndex(i);
 					position.NextProperty();
-					position.SplitHorizontal(EditorGUIUtility.labelWidth, out var keyRect, out var valueRect, 0);
-					valueRect.xMin += 4;
-					valueRect.SplitHorizontalRight(16, out valueRect, out var btnRect, 0);
-					valueRect.width -= 4;
-
-					lColor = GUI.color;
-					if(duplicates.Contains(k)) GUI.color = errorColor;
-					PropertyDrawerUtility.DrawPropertyDirect(keyRect, GUIContent.none, k, preferMonospaceKeys);
-					//EditorGUI.PropertyField(keyRect, k, GUIContent.none);
-					GUI.color = lColor;
-
-					EditorGUI.indentLevel = 0;
-					//TODO: currently only supports the first visible property
-					v.isExpanded = true;
-					if(v.hasVisibleChildren && v.propertyType == SerializedPropertyType.ManagedReference) v.NextVisible(true);
-					if(v.propertyType == SerializedPropertyType.ManagedReference && PropertyDrawerUtility.GetTargetObjectOfProperty(v) == null)
-					{
-						GUI.Label(valueRect, "(null)");
-					}
-					else
-					{
-						valueRect.xMin -= 4;
-						var lLW = EditorGUIUtility.labelWidth;
-						EditorGUIUtility.labelWidth = 4;
-						var content = new GUIContent(" ");
-						if(v.type == "Vector4")
-						{
-							v.vector4Value = EditorGUI.Vector4Field(valueRect, content, v.vector4Value);
-						}
-						else
-						{
-							EditorGUI.PropertyField(valueRect, v, content, false);
-						}
-						EditorGUIUtility.labelWidth = lLW;
-					}
-					EditorGUI.indentLevel = indentLevel;
-
-					if(GUI.Button(btnRect, GUIContent.none, EditorStyles.foldoutHeaderIcon))
-					{
-						int index = i;
-						var menu = new GenericMenu();
-						string path = property.propertyPath;
-						menu.AddItem("Move Up", index > 0, false, () =>
-						{
-							so.Update();
-							so.FindProperty(path + "._keys").MoveArrayElement(index, index - 1);
-							so.FindProperty(path + "._values").MoveArrayElement(index, index - 1);
-							so.ApplyModifiedProperties();
-						});
-						menu.AddItem("Move Down", index < keys.arraySize - 1, false, () =>
-						{
-							so.Update();
-							so.FindProperty(path + "._keys").MoveArrayElement(index, index + 1);
-							so.FindProperty(path + "._values").MoveArrayElement(index, index + 1);
-							so.ApplyModifiedProperties();
-						});
-						if(polymorphic)
-						{
-							string root = "Change Element Type/";
-							foreach(var t in polymorphicTypes[dictionaryType])
-							{
-								var currentType = PropertyDrawerUtility.GetTargetObjectOfProperty(v)?.GetType();
-								menu.AddItem(root + GetTypeName(t), true, currentType == t, () =>
-								{
-									Undo.RecordObject(so.targetObject, "Change Element Type");
-									target.GetType().GetProperties().First(x => x.GetIndexParameters().Length > 0)
-										.SetValue(target, Activator.CreateInstance(t), new object[] { kObj });
-								});
-							}
-						}
-						menu.AddItem("Delete Element", true, false, () => DeleteItem(target, property, kObj, index));
-						menu.ShowAsContext();
-					}
+					lColor = DrawElement(position, property, target, preferMonospaceKeys, dictionaryType, polymorphic, indentLevel, so, keys, duplicates, i, k, kObj, v);
 				}
 				position.NextProperty();
-				position.SplitHorizontalRight(16, out _, out var addRect);
-				if(polymorphic)
-				{
-					if(GUI.Button(addRect, EditorGUIUtility.IconContent("d_Toolbar Plus More"), "IconButton"))
-					{
-						var menu = new GenericMenu();
-						foreach(var t in polymorphicTypes[dictionaryType])
-						{
-							menu.AddItem(new GUIContent(GetTypeName(t)), false,
-							() =>
-							{
-								AddItem(target, so, t);
-							}
-							);
-						}
-						if(menu.GetItemCount() == 0) menu.AddDisabledItem(new GUIContent("None"));
-						menu.ShowAsContext();
-					}
-					addRect.x -= addRect.width;
-					if(GUI.Button(addRect, GUIContent.none, EditorStyles.foldoutHeaderIcon))
-					{
-						var menu = new GenericMenu();
-						menu.AddItem("Clear Dictionary", true, false, () =>
-						{
-							target.GetType().GetMethod("Clear").Invoke(target, new object[] { });
-						});
-						menu.ShowAsContext();
-					}
-				}
-				else
-				{
-					if(GUI.Button(addRect, EditorGUIUtility.IconContent("d_Toolbar Plus"), "IconButton"))
-					{
-						var valueType = ((IUnityDictionary)target).ValueType;
-						EditorApplication.delayCall += () => AddItem(target, so, valueType);
-					}
-				}
+				DrawAddButton(position, target, dictionaryType, polymorphic, so);
 				EditorGUI.indentLevel--;
 			}
 			EditorGUI.EndProperty();
+		}
+
+		private static void DrawAddButton(Rect position, object target, Type dictionaryType, bool polymorphic, SerializedObject so)
+		{
+			position.SplitHorizontalRight(16, out _, out var addRect);
+			if(polymorphic)
+			{
+				if(GUI.Button(addRect, EditorGUIUtility.IconContent("d_Toolbar Plus More"), "IconButton"))
+				{
+					var menu = new GenericMenu();
+					foreach(var t in polymorphicTypes[dictionaryType])
+					{
+						menu.AddItem(new GUIContent(GetTypeName(t)), false,
+						() =>
+						{
+							AddItem(target, so, t);
+						}
+						);
+					}
+					if(menu.GetItemCount() == 0) menu.AddDisabledItem(new GUIContent("None"));
+					menu.ShowAsContext();
+				}
+				addRect.x -= addRect.width;
+				if(GUI.Button(addRect, GUIContent.none, EditorStyles.foldoutHeaderIcon))
+				{
+					var menu = new GenericMenu();
+					menu.AddItem("Clear Dictionary", true, false, () =>
+					{
+						target.GetType().GetMethod("Clear").Invoke(target, new object[] { });
+					});
+					menu.ShowAsContext();
+				}
+			}
+			else
+			{
+				if(GUI.Button(addRect, EditorGUIUtility.IconContent("d_Toolbar Plus"), "IconButton"))
+				{
+					var valueType = ((IUnityDictionary)target).ValueType;
+					EditorApplication.delayCall += () => AddItem(target, so, valueType);
+				}
+			}
+		}
+
+		private static Color DrawElement(Rect position, SerializedProperty property, object target, bool preferMonospaceKeys, Type dictionaryType, bool polymorphic, int indentLevel, SerializedObject so, SerializedProperty keys, List<object> duplicates, int i, SerializedProperty k, object kObj, SerializedProperty v)
+		{
+			Color lColor;
+			position.SplitHorizontal(EditorGUIUtility.labelWidth, out var keyRect, out var valueRect, 0);
+			valueRect.xMin += 4;
+			valueRect.SplitHorizontalRight(16, out valueRect, out var btnRect, 0);
+			valueRect.width -= 4;
+
+			lColor = GUI.color;
+			if(duplicates.Contains(k)) GUI.color = errorColor;
+			PropertyDrawerUtility.DrawPropertyDirect(keyRect, GUIContent.none, k, preferMonospaceKeys);
+			//EditorGUI.PropertyField(keyRect, k, GUIContent.none);
+			GUI.color = lColor;
+
+			EditorGUI.indentLevel = 0;
+			//TODO: currently only supports the first visible property
+			v.isExpanded = true;
+			if(v.hasVisibleChildren && v.propertyType == SerializedPropertyType.ManagedReference) v.NextVisible(true);
+			if(v.propertyType == SerializedPropertyType.ManagedReference && PropertyDrawerUtility.GetTargetObjectOfProperty(v) == null)
+			{
+				GUI.Label(valueRect, "(null)");
+			}
+			else
+			{
+				valueRect.xMin -= 4;
+				var lLW = EditorGUIUtility.labelWidth;
+				EditorGUIUtility.labelWidth = 4;
+				var content = new GUIContent(" ");
+				if(v.type == "Vector4")
+				{
+					v.vector4Value = EditorGUI.Vector4Field(valueRect, content, v.vector4Value);
+				}
+				else
+				{
+					EditorGUI.PropertyField(valueRect, v, content, false);
+				}
+				EditorGUIUtility.labelWidth = lLW;
+			}
+			EditorGUI.indentLevel = indentLevel;
+
+			if(GUI.Button(btnRect, GUIContent.none, EditorStyles.foldoutHeaderIcon))
+			{
+				int index = i;
+				var menu = new GenericMenu();
+				string path = property.propertyPath;
+				menu.AddItem("Move Up", index > 0, false, () =>
+				{
+					so.Update();
+					so.FindProperty(path + "._keys").MoveArrayElement(index, index - 1);
+					so.FindProperty(path + "._values").MoveArrayElement(index, index - 1);
+					so.ApplyModifiedProperties();
+				});
+				menu.AddItem("Move Down", index < keys.arraySize - 1, false, () =>
+				{
+					so.Update();
+					so.FindProperty(path + "._keys").MoveArrayElement(index, index + 1);
+					so.FindProperty(path + "._values").MoveArrayElement(index, index + 1);
+					so.ApplyModifiedProperties();
+				});
+				if(polymorphic)
+				{
+					string root = "Change Element Type/";
+					foreach(var t in polymorphicTypes[dictionaryType])
+					{
+						var currentType = PropertyDrawerUtility.GetTargetObjectOfProperty(v)?.GetType();
+						menu.AddItem(root + GetTypeName(t), true, currentType == t, () =>
+						{
+							Undo.RecordObject(so.targetObject, "Change Element Type");
+							target.GetType().GetProperties().First(x => x.GetIndexParameters().Length > 0)
+								.SetValue(target, Activator.CreateInstance(t), new object[] { kObj });
+						});
+					}
+				}
+				menu.AddItem("Delete Element", true, false, () => DeleteItem(target, property, kObj, index));
+				menu.ShowAsContext();
+			}
+
+			return lColor;
 		}
 
 		private static void AddItem(object target, SerializedObject so, Type valueType)
