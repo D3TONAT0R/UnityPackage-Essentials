77d5447e303092c6b3f792b0dd59a3c2f25abfd3
D3TONAT0R <gdv@swissonline.ch>
2024-07-11 10:42:54 +0200

    Create AssetDatabaseUtility

---
+++ Editor/AssetDatabaseUtility.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor.ProjectWindowCallback;
+using UnityEditor;
+using System.IO;
+using System;
+using Object = UnityEngine.Object;
+
+namespace D3TEditor
+{
+	public static class AssetDatabaseUtility
+	{
+		internal class CreateFromTemplateAssetAction : EndNameEditAction
+		{
+			private string templateFile;
+			private Action<Object> callback;
+
+			public static CreateFromTemplateAssetAction CreateAction(string templateFile, Action<Object> callback)
+			{
+				var instance = CreateInstance<CreateFromTemplateAssetAction>();
+				instance.templateFile = templateFile;
+				instance.callback = callback;
+				return instance;
+			}
+
+			public override void Action(int instanceId, string pathName, string resourceFile)
+			{
+				string finalPath = AssetDatabase.GenerateUniqueAssetPath(pathName + Path.GetExtension(templateFile));
+				AssetDatabase.CopyAsset(templateFile, finalPath);
+				var newAsset = AssetDatabase.LoadAssetAtPath<Texture2D>(finalPath);
+				ProjectWindowUtil.ShowCreatedAsset(newAsset);
+				callback?.Invoke(newAsset);
+			}
+		}
+
+		/// <summary>
+		/// Prompts the user to type a name for a new asset that will be created from the given template file.
+		/// </summary>
+		public static void BeginAssetCreationFromTemplateFile(string templateAssetPath, string defaultName, Texture2D icon, Action<Object> callback = null)
+		{
+			var action = CreateFromTemplateAssetAction.CreateAction(templateAssetPath, callback);
+			ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, action, defaultName, icon, templateAssetPath);
+		}
+	}
+}
--- Editor/MenuCommands/CreateTexture2DMenuItems.cs
+++ Editor/MenuCommands/CreateTexture2DMenuItems.cs
@@ -7,26 +7,6 @@ namespace D3TEditor
 {
 	internal static class CreateTexture2DMenuItems
 	{
-		internal class CreateTexture2DAssetAction : EndNameEditAction
-		{
-			private string templateFile;
-
-			public static CreateTexture2DAssetAction CreateAction(string templateFile)
-			{
-				var instance = CreateInstance<CreateTexture2DAssetAction>();
-				instance.templateFile = templateFile;
-				return instance;
-			}
-
-			public override void Action(int instanceId, string pathName, string resourceFile)
-			{
-				string finalPath = AssetDatabase.GenerateUniqueAssetPath(pathName + Path.GetExtension(templateFile));
-				AssetDatabase.CopyAsset(templateFile, finalPath);
-				var newAsset = AssetDatabase.LoadAssetAtPath<Texture2D>(finalPath);
-				ProjectWindowUtil.ShowCreatedAsset(newAsset);
-			}
-		}
-
 		private const int PRIORITY = 303;
 
 		[MenuItem("Assets/Create/Texture2D/Blank PSD Texture", priority = PRIORITY)]
@@ -44,24 +24,8 @@ namespace D3TEditor
 		private static void CreateBlankAsset(string extension, string newName)
 		{
 			string templatePath = $"Packages/com.github.d3tonat0r.essentials/Editor/TemplateAssets/template_{extension}.{extension}";
-			var icon = (EditorGUIUtility.IconContent("Texture2D Icon").image as Texture2D);
-			ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, CreateTexture2DAssetAction.CreateAction(templatePath), newName, icon, templatePath);
-		}
-
-		private static string GetSelectedPathOrFallback()
-		{
-			string path = "Assets";
-
-			foreach(Object obj in Selection.GetFiltered(typeof(Object), SelectionMode.Assets))
-			{
-				path = AssetDatabase.GetAssetPath(obj);
-				if(!string.IsNullOrEmpty(path) && File.Exists(path))
-				{
-					path = Path.GetDirectoryName(path);
-					break;
-				}
-			}
-			return path;
+			var icon = EditorGUIUtility.IconContent("d_Texture2D Icon").image as Texture2D;
+			AssetDatabaseUtility.BeginAssetCreationFromTemplateFile(templatePath, newName, icon);
 		}
 	}
 }
