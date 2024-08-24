0facb2e6c3700a21c850ee2d2460e17f07aa30ec
D3TONAT0R <gdv@swissonline.ch>
2024-08-25 00:09:21 +0200

    Add inlined layout control property

--- Editor/PropertyDrawers/InlineClassDrawer.cs
+++ Editor/PropertyDrawers/InlineClassDrawer.cs
@@ -11,27 +11,41 @@ namespace D3TEditor
 	{
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
-			position.height = EditorGUIUtility.singleLineHeight;
-			EditorGUI.BeginProperty(position, label, property);
-
-			EditorGUI.LabelField(position, label);
-			position.xMin += EditorGUIUtility.labelWidth + 3;
-
-			int childCount = GetDirectChildren(property).Count();
-			var rects = position.DivideHorizontal(childCount, 2);
-			int i = 0;
-			while(property.NextVisible(i == 0) && i < childCount)
+			var target = PropertyDrawerUtility.GetTargetObjectOfProperty<InlineClass>(property);
+			if(target.InheritInlinedLayout)
 			{
-				PropertyDrawerUtility.DrawPropertyField(rects[i], property, GUIContent.none);
-				i++;
-			}
+				position.height = EditorGUIUtility.singleLineHeight;
 
-			EditorGUI.EndProperty();
+				EditorGUI.LabelField(position, label);
+				position.xMin += EditorGUIUtility.labelWidth + 3;
+
+				int childCount = GetDirectChildren(property).Count();
+				var rects = position.DivideHorizontal(childCount, 2);
+				int i = 0;
+				while(property.NextVisible(i == 0) && i < childCount)
+				{
+					EditorGUI.PropertyField(rects[i], property, GUIContent.none);
+					//PropertyDrawerUtility.DrawPropertyField(rects[i], prop, GUIContent.none);
+					i++;
+				}
+			}
+			else
+			{
+				EditorGUI.PropertyField(position, property, label, true);
+			}
 		}
 
 		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
 		{
-			return EditorGUIUtility.singleLineHeight;
+			var target = PropertyDrawerUtility.GetTargetObjectOfProperty<InlineClass>(property);
+			if(target.InheritInlinedLayout)
+			{
+				return EditorGUIUtility.singleLineHeight;
+			}
+			else
+			{
+				return EditorGUI.GetPropertyHeight(property, label, true);
+			}
 		}
 
 		private static IEnumerable<SerializedProperty> GetDirectChildren(SerializedProperty parent)
