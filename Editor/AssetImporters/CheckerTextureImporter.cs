3c218c4a42469d2df67fcee962ce30579468886f
D3TONAT0R <gdv@swissonline.ch>
2024-09-08 19:10:49 +0200

    Create scripted asset generator base class

--- Editor/AssetImporters/CheckerTextureImporter.cs
+++ Editor/AssetImporters/CheckerTextureImporter.cs
@@ -8,53 +8,30 @@ using UnityEngine;
 namespace D3TEditor
 {
 	[ScriptedImporter(0, "checker")]
-	internal class CheckerTextureImporter : ScriptedImporter
+	internal class CheckerTextureImporter : ScriptedTextureGenerator
 	{
-
-		public Vector2Int resolution = new Vector2Int(128, 128);
-
+		[Space(20)]
 		public int pixelsPerSquare = 64;
 		public int border = 0;
 		public Color color1 = Color.black;
 		public Color color2 = Color.white;
 		public Color borderColor = Color.gray;
 
-		[Space(10)]
-		public bool linear = false;
-		public bool generateMipMaps = true;
-		public TextureWrapMode wrapMode = TextureWrapMode.Clamp;
-		public FilterMode filterMode = FilterMode.Bilinear;
-
-		public override void OnImportAsset(AssetImportContext ctx)
+		protected override Color GetPixelColor(int x, int y, Vector2 uv)
 		{
-			resolution.x = Mathf.Clamp(resolution.x, 1, 4096);
-			resolution.y = Mathf.Clamp(resolution.y, 1, 4096);
-			var texture = new Texture2D(resolution.x, resolution.y, TextureFormat.RGBA32, generateMipMaps, linear);
-			texture.alphaIsTransparency = true;
-			texture.wrapMode = wrapMode;
-			texture.filterMode = filterMode;
-
-			for(int x = 0; x < resolution.x; x++)
+			Color color;
+			int xMod = x % pixelsPerSquare;
+			int yMod = y % pixelsPerSquare;
+			if(xMod < border || yMod < border || xMod >= pixelsPerSquare - border || yMod >= pixelsPerSquare - border)
 			{
-				for(int y = 0; y < resolution.y; y++)
-				{
-					Color color;
-					int xMod = x % pixelsPerSquare;
-					int yMod = y % pixelsPerSquare;
-					if(xMod < border || yMod < border || xMod >= pixelsPerSquare - border || yMod >= pixelsPerSquare - border)
-					{
-						color = borderColor;
-					}
-					else
-					{
-						bool c2 = x / pixelsPerSquare % 2 == y / pixelsPerSquare % 2;
-						color = c2 ? color2 : color1;
-					}
-					texture.SetPixel(x, y, color);
-				}
+				color = borderColor;
 			}
-			texture.Apply();
-			ctx.AddObjectToAsset("texture", texture);
+			else
+			{
+				bool c2 = x / pixelsPerSquare % 2 == y / pixelsPerSquare % 2;
+				color = c2 ? color2 : color1;
+			}
+			return color;
 		}
 	}
 }
