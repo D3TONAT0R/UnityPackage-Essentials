6556a0e25adbbfcdc7abc72ef6fd792c70eabd82
D3TONAT0R <gdv@swissonline.ch>
2024-10-27 14:55:01 +0100

    Rewrite build post processor

--- Editor/BuildProcessors/MacBuildPostProcessor.cs
+++ Editor/BuildProcessors/MacBuildPostProcessor.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using UnityEditor;
@@ -9,26 +11,49 @@ namespace D3TEditor.BuildProcessors
 {
 	public class MacBuildPostProcessor : IPostprocessBuildWithReport
 	{
-		const string TEMPLATE_ARCHIVE_PATH = "Packages/UnityPackage-Essentials/Editor/BuildProcessors/MacOSExecutableTemplate.zip";
-		const string EXECUTABLE_NAME = "TemplateExecutable";
+		const int X_OWNER_BIT = 1 << 22;
+		const int X_GROUP_BIT = 1 << 19;
+		const int X_OTHER_BIT = 1 << 16;
+		const int ALL_X_BITS = X_OWNER_BIT | X_GROUP_BIT | X_OTHER_BIT;
 
-		public int callbackOrder => 0;
+		public int callbackOrder => int.MaxValue;
 
 		public void OnPostprocessBuild(BuildReport report)
 		{
 			if(report.summary.platform == BuildTarget.StandaloneOSX)
 			{
 				var rootPath = report.summary.outputPath;
-				var executableFilePath = rootPath + "/Contents/MacOS/" + PlayerSettings.productName;
-				//Copy contents of the executable file
-				var builtData = File.ReadAllBytes(executableFilePath);
-				File.Delete(executableFilePath);
-				//Unzip archive containing the executable file with the x attribute set
-				ZipFile.ExtractToDirectory(TEMPLATE_ARCHIVE_PATH, rootPath + "/Contents/MacOS/");
-				File.Move(rootPath + "/Contents/MacOS/" + EXECUTABLE_NAME, executableFilePath);
-				//Rewrite contents of the executable file
-				File.WriteAllBytes(executableFilePath, builtData);
-				Debug.Log("MacOS executable successfully fixed.");
+				var executableFilePath = "Contents/MacOS/" + PlayerSettings.productName;
+
+				//Delete existing zip if present
+				if(File.Exists(rootPath + ".zip"))
+				{
+					File.Delete(rootPath + ".zip");
+				}
+				//Compress executable into a zip file
+				ZipFile.CreateFromDirectory(rootPath, rootPath + ".zip");
+
+				//Modify zip to set the x flag
+				using(var zip = ZipFile.Open(rootPath + ".zip", ZipArchiveMode.Update))
+				{
+					zip.GetEntry(executableFilePath).ExternalAttributes |= ALL_X_BITS;
+				}
+
+				//Self test
+				//PerformAttributeTest(rootPath, executableFilePath);
+
+				//Delete original build directory
+				Directory.Delete(rootPath, true);
+			}
+		}
+
+		private static void PerformAttributeTest(string rootPath, string executableFilePath)
+		{
+			using(var zip = ZipFile.OpenRead(rootPath + ".zip"))
+			{
+				bool test = (zip.GetEntry(executableFilePath).ExternalAttributes & ALL_X_BITS) == ALL_X_BITS;
+				if(test) Debug.Log("Perms test passed");
+				else Debug.LogError("Perms test failed: "+Convert.ToString(zip.GetEntry(executableFilePath).ExternalAttributes, 2));
 			}
 		}
 	} 
