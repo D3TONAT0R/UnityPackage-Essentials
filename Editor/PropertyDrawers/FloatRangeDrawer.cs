573300d923d0cee6192747587f598da1438fe4f7
D3TONAT0R <gdv@swissonline.ch>
2024-12-17 13:27:44 +0100

    Disable clamping

--- Editor/PropertyDrawers/FloatRangeDrawer.cs
+++ Editor/PropertyDrawers/FloatRangeDrawer.cs
@@ -12,28 +12,17 @@ namespace UnityEssentialsEditor.PropertyDrawers
 			EditorGUI.BeginProperty(position, label, property);
 			EditorGUI.showMixedValue = property.hasMultipleDifferentValues;
 			position.height = EditorGUIUtility.singleLineHeight;
-			var min = property.FindPropertyRelative("min");
-			var max = property.FindPropertyRelative("max");
-			var attr = GetRangeAttribute(property);
-			if(attr != null)
+			var min = property.FindPropertyRelative(nameof(FloatRange.min));
+			var max = property.FindPropertyRelative(nameof(FloatRange.max));
+			var rangeAttribute = GetRangeAttribute(property);
+			if(rangeAttribute != null)
 			{
-				var minValue = min.floatValue;
-				var maxValue = max.floatValue;
-				EditorGUI.BeginChangeCheck();
-				EditorGUI.showMixedValue = min.hasMultipleDifferentValues || max.hasMultipleDifferentValues;
-				EditorGUI.MinMaxSlider(position, label, ref minValue, ref maxValue, attr.min, attr.max);
-				if(EditorGUI.EndChangeCheck())
-				{
-					minValue = Mathf.Min(minValue, maxValue);
-					maxValue = Mathf.Max(minValue, maxValue);
-					min.floatValue = minValue;
-					max.floatValue = maxValue;
-				}
+				DrawSlider(position, label, min, max, rangeAttribute);
 				position.NextProperty();
 			}
 			bool hasLabel = !string.IsNullOrEmpty(label.text);
 			position.SplitHorizontal(hasLabel ? EditorGUIUtility.labelWidth : 0, out var labelRect, out position);
-			if(attr == null)
+			if(rangeAttribute == null)
 			{
 				EditorGUI.LabelField(labelRect, label);
 			}
@@ -42,27 +31,46 @@ namespace UnityEssentialsEditor.PropertyDrawers
 			EditorGUI.indentLevel = 0;
 			EditorGUIUtility.labelWidth = 30;
 
-			EditorGUI.BeginChangeCheck();
-			EditorGUI.showMixedValue = min.hasMultipleDifferentValues;
-			EditorGUI.PropertyField(minRect, min);
-			if(EditorGUI.EndChangeCheck())
-			{
-				min.floatValue = Mathf.Min(min.floatValue, max.floatValue);
-				if(attr != null) min.floatValue = Mathf.Clamp(min.floatValue, attr.min, attr.max);
-			}
-
-			EditorGUI.BeginChangeCheck();
-			EditorGUI.showMixedValue = max.hasMultipleDifferentValues;
-			EditorGUI.PropertyField(maxRect, max);
-			if(EditorGUI.EndChangeCheck())
-			{
-				max.floatValue = Mathf.Max(min.floatValue, max.floatValue);
-				if(attr != null) max.floatValue = Mathf.Clamp(max.floatValue, attr.min, attr.max);
-			}
+			DrawField(min, max, minRect, rangeAttribute, false);
+			DrawField(max, min, maxRect, rangeAttribute, true);
 
 			EditorGUI.indentLevel = indent;
 			EditorGUI.showMixedValue = false;
 			EditorGUI.EndProperty();
+		}
+
+		private static void DrawField(SerializedProperty prop, SerializedProperty other, Rect minRect, MinMaxRangeAttribute rangeAttribute, bool max)
+		{
+			EditorGUI.BeginChangeCheck();
+			EditorGUI.showMixedValue = prop.hasMultipleDifferentValues;
+			EditorGUI.PropertyField(minRect, prop);
+			if(EditorGUI.EndChangeCheck())
+			{
+				if(rangeAttribute != null)
+				{
+					float value = prop.floatValue;
+					value = Mathf.Clamp(value, rangeAttribute.min, rangeAttribute.max);
+					value = max ? Mathf.Max(value, other.floatValue) : Mathf.Min(value, other.floatValue);
+					prop.floatValue = value;
+				}
+			}
+		}
+
+		private static void DrawSlider(Rect position, GUIContent label, SerializedProperty min, SerializedProperty max,
+			MinMaxRangeAttribute attr)
+		{
+			var minValue = min.floatValue;
+			var maxValue = max.floatValue;
+			EditorGUI.BeginChangeCheck();
+			EditorGUI.showMixedValue = min.hasMultipleDifferentValues || max.hasMultipleDifferentValues;
+			EditorGUI.MinMaxSlider(position, label, ref minValue, ref maxValue, attr.min, attr.max);
+			if(EditorGUI.EndChangeCheck())
+			{
+				minValue = Mathf.Min(minValue, maxValue);
+				maxValue = Mathf.Max(minValue, maxValue);
+				min.floatValue = minValue;
+				max.floatValue = maxValue;
+			}
 		}
 
 		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
