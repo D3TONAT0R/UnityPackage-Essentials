54f2f20c373d5dc012899f06e3d54028cdb913e1
D3TONAT0R <gdv@swissonline.ch>
2025-09-26 19:37:26 +0200

    Include GetComponent extensions on gameObject

--- Runtime/Extensions/ComponentExtensions.cs
+++ Runtime/Extensions/ComponentExtensions.cs
@@ -20,11 +20,36 @@ namespace UnityEssentials
 		}
 
 		/// <summary>
+		/// Gets the component of the given type attached to this GameObject. If it doesn't exist, a new one is created.
+		/// </summary>
+		public static T GetOrAddComponent<T>(this GameObject g) where T : Component
+		{
+			if(g.TryGetComponent<T>(out var comp))
+			{
+				return comp;
+			}
+			else
+			{
+				return g.gameObject.AddComponent<T>();
+			}
+		}
+
+		/// <summary>
 		/// Gets the component of the given type attached to this GameObject. If it doesn't exist, an exception is thrown.
 		/// </summary>
 		public static T GetRequiredComponent<T>(this Component c)
 		{
 			T comp = c.GetComponent<T>();
+			if(comp == null) throw new System.NullReferenceException($"Could not find required component {typeof(T).Name} on GameObject '{c.name}'.");
+			return comp;
+		}
+
+		/// <summary>
+		/// Gets the component of the given type attached to this GameObject. If it doesn't exist, an exception is thrown.
+		/// </summary>
+		public static T GetRequiredComponent<T>(this GameObject g)
+		{
+			T comp = g.GetComponent<T>();
 			if(comp == null) throw new System.NullReferenceException($"Could not find required component {typeof(T).Name} on GameObject '{c.name}'.");
 			return comp;
 		}
