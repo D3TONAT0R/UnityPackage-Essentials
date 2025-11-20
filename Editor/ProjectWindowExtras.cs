90de4538a52eae5a048ac45bd76c5354d51fa260
D3TONAT0R <gdv@swissonline.ch>
2025-11-20 01:19:23 +0100

    Highlight currently open prefab in project window (fixes #41)

---
+++ Editor/ProjectWindowExtras.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEssentials;
+
+namespace UnityEssentialsEditor
+{
+	public static class ProjectWindowExtras
+	{
+		private static GUIStyle borderStyle;
+		private static GUIContent prefabModeContent = new GUIContent("", "Currently editing prefab");
+
+		[InitializeOnLoadMethod]
+		private static void Init()
+		{
+			EditorApplication.projectWindowItemOnGUI = ProjectWindowItemOnGUI;
+			var borderTexture = AssetDatabase.LoadAssetAtPath<Texture2D>(Path.Combine(EssentialsPackageInfo.ROOT_PATH, "Editor", "EditorAssets", "border_box.png"));
+			borderStyle = new GUIStyle
+			{
+				normal =
+				{
+					background = borderTexture
+				},
+				border = new RectOffset(4, 4, 4, 4)
+			};
+		}
+
+		private static void ProjectWindowItemOnGUI(string guid, Rect pos)
+		{
+			if(Event.current.type != EventType.Repaint)
+			{
+				return;
+			}
+			// Check if we are in prefab mode
+			var prefabStage = PrefabStageUtility.GetCurrentPrefabStage();
+			if (prefabStage != null)
+			{
+				var path = AssetDatabase.GUIDToAssetPath(guid);
+				if (path == prefabStage.assetPath)
+				{
+					// Draw a border indicating we are in prefab mode
+					bool listView = pos.height <= 32;
+					if(listView)
+					{
+						pos = pos.Outset(1);
+						pos.width -= 1;
+					}
+					else
+					{
+						// Add extra offset when in grid view
+						pos = pos.Outset(4);
+					}
+					GUI.color = new Color(0.24f, 0.37f, 0.59f, 1f).Lighten(0.2f);
+					GUI.Label(pos, prefabModeContent, borderStyle);
+					GUI.color = Color.white;
+				}
+			}
+		}
+	}
+}
---
+++ Runtime/EssentialsPackageInfo.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+
+namespace UnityEssentials
+{
+	public static class EssentialsPackageInfo
+	{
+		public const string ROOT_PATH = "Packages/com.github.d3tonat0r.essentials";
+	}
+}
