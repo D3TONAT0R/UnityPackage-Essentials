b1b0f04bfad8a596fc5ffbe7173212ea0c1eb1f9
D3TONAT0R <gdv@swissonline.ch>
2026-05-01 23:01:12 +0200

    Object reference value type

---
+++ Editor/PropertyDrawers/ObjectReferenceDrawer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEssentials;
+
+namespace UnityEssentialsEditor.PropertyDrawers
+{
+	[CustomPropertyDrawer(typeof(ObjectReference<>))]
+	public class ObjectReferenceDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			EditorGUI.BeginProperty(position, label, property);
+			var scriptRefProp = property.FindPropertyRelative("objectRef");
+			var type = property.GetValueType().GetGenericArguments()[0];
+			var newValue = EditorGUI.ObjectField(position, label, scriptRefProp.objectReferenceValue, type, true);
+			if (newValue != scriptRefProp.objectReferenceValue)
+			{
+				scriptRefProp.objectReferenceValue = newValue;
+			}
+			EditorGUI.EndProperty();
+		}
+	}
+}
