755b3d49b1aa43083eb38560655befb0c59330aa
D3TONAT0R <gdv@swissonline.ch>
2026-07-09 01:20:17 +0200

    Add dynamic range attribute

---
+++ Editor/PropertyDrawers/DynamicRangeAttributeDrawer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEssentials;
+
+namespace UnityEssentialsEditor.PropertyDrawers
+{
+	[CustomPropertyDrawer(typeof(DynamicRangeAttribute), true)]
+	public class DynamicRangeAttributeDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			if (!PropertyDrawerUtility.ValidatePropertyTypeForAttribute(position, property, label, SerializedPropertyType.Float,
+				    SerializedPropertyType.Integer))
+			{
+				return;
+			}
+			var attr = attribute as DynamicRangeAttribute;
+			EditorGUI.BeginProperty(position, label, property);
+			EditorGUI.BeginChangeCheck();
+			var target = property.GetParentObject();
+			var min = GetLimitValue(target, attr.minPropertyName, attr.minValue);
+			var max = GetLimitValue(target, attr.maxPropertyName, attr.maxValue);
+			if (!attr.softLimits && !Application.isPlaying)
+			{
+				if(property.floatValue < min ||  property.floatValue > max) 
+					property.floatValue = Mathf.Clamp(property.floatValue, min, max);
+			}
+			//TODO: allow setting values outside of slider range when soft limits is set to true
+			if (property.propertyType == SerializedPropertyType.Float)
+			{
+				var value = property.floatValue;
+				value = EditorGUI.Slider(position, label, value, min, max);
+				if (EditorGUI.EndChangeCheck()) property.floatValue = value;
+			}
+			else
+			{
+				var value = (float)property.intValue;
+				value = EditorGUI.Slider(position, label, value, min, max);
+				if (EditorGUI.EndChangeCheck()) property.intValue = (int)value;
+			}
+			EditorGUI.EndProperty();
+		}
+
+		private float GetLimitValue(object source, string propertyName, float value)
+		{
+			if (string.IsNullOrEmpty(propertyName)) return value;
+			var member = source.GetType().GetMember(propertyName)[0];
+			var v = ReflectionUtility.GetMemberValue(member, source);
+			return Convert.ToSingle(v);
+		}
+	}
+}
---
+++ Runtime/Attributes/DynamicRangeAttribute.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace UnityEssentials
+{
+	public class DynamicRangeAttribute : PropertyAttribute
+	{
+		public string minPropertyName;
+		public float minValue;
+		public string maxPropertyName;
+		public float maxValue;
+		public bool softLimits;
+
+		public DynamicRangeAttribute(float minValue, float maxValue, bool softLimits = false)
+		{
+			this.minValue = minValue;
+			this.maxValue = maxValue;
+			this.softLimits = softLimits;
+		}
+		
+		public DynamicRangeAttribute(string minPropertyName, string maxPropertyName, bool softLimits = false)
+		{
+			this.minPropertyName = minPropertyName;
+			this.maxPropertyName = maxPropertyName;
+			this.softLimits = softLimits;
+		}
+		
+		public DynamicRangeAttribute(string minPropertyName, float maxValue, bool softLimits = false)
+		{
+			this.minPropertyName = minPropertyName;
+			this.maxValue = maxValue;
+			this.softLimits = softLimits;
+		}
+		
+		public DynamicRangeAttribute(float minValue, string maxPropertyName, bool softLimits = false)
+		{
+			this.minValue = minValue;
+			this.maxPropertyName = maxPropertyName;
+			this.softLimits = softLimits;
+		}
+	}
+}
