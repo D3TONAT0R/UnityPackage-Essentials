50a965d1fb4531570a9fbe00d821b623bb325d10
D3TONAT0R <gdv@swissonline.ch>
2026-08-22 00:05:25 +0200

    Refactor RandomUtility

--- Runtime/RandomUtility.cs
+++ Runtime/RandomUtility.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace UnityEssentials
 {
@@ -9,7 +11,9 @@ namespace UnityEssentials
 	/// </summary>
 	public static class RandomUtility
 	{
-
+		[ThreadStatic]
+		private static List<int> indices;
+		
 		/// <summary>
 		/// Generates a random boolean using the given probability.
 		/// </summary>
@@ -67,23 +71,6 @@ namespace UnityEssentials
 		}
 
 		/// <summary>
-		/// Returns a random integer between min (inclusive) and max (exclusive), excluding the given integers.
-		/// </summary>
-		public static int RangeExcluding(int min, int max, params int[] exclude)
-		{
-			if(max - min <= 0) return -1;
-			var array = Enumerable.Range(min, max - min).Except(exclude).ToArray();
-			if(array.Length > 0)
-			{
-				return Random.Range(0, array.Length);
-			}
-			else
-			{
-				return -1;
-			}
-		}
-
-		/// <summary>
 		/// Picks a random item from the given array.
 		/// </summary>
 		/// <returns>A random item from the array.</returns>
@@ -105,18 +92,7 @@ namespace UnityEssentials
 		/// Picks a random item from the given array, excluding items at the given indices.
 		/// </summary>
 		/// <returns>A random item from the array.</returns>
-		public static T PickRandomExcluding<T>(T[] array, params int[] excludeIndices)
-		{
-			int index = PickRandomIndexExcluding(array.Length, excludeIndices);
-			if(index >= 0) return array[index];
-			else return default;
-		}
-
-		/// <summary>
-		/// Picks a random item from the given list, excluding items at the given indices.
-		/// </summary>
-		/// <returns>A random item from the list.</returns>
-		public static T PickRandomExcluding<T>(List<T> list, params int[] excludeIndices)
+		public static T PickRandomExcluding<T>(IList<T> list, params int[] excludeIndices)
 		{
 			int index = PickRandomIndexExcluding(list.Count, excludeIndices);
 			if(index >= 0) return list[index];
@@ -129,10 +105,12 @@ namespace UnityEssentials
 		/// <returns>A random item from the array.</returns>
 		public static int PickRandomIndexExcluding(int length, params int[] excludeIndices)
 		{
-			var indices = Enumerable.Range(0, length).Except(excludeIndices).ToArray();
-			if(indices.Length > 0)
+			indices ??= new List<int>();
+			indices.Clear();
+			indices.AddRange(Enumerable.Range(0, length).Except(excludeIndices));
+			if(indices.Count > 0)
 			{
-				return indices[Random.Range(0, indices.Length)];
+				return PickRandom(indices);
 			}
 			else
 			{
@@ -143,7 +121,7 @@ namespace UnityEssentials
 		/// <summary>
 		/// Picks a random item and removes it from the list.
 		/// </summary>
-		public static T TakeRandomItem<T>(List<T> list)
+		public static T TakeRandomItem<T>(IList<T> list)
 		{
 			int i = Random.Range(0, list.Count);
 			var item = list[i];
@@ -156,12 +134,12 @@ namespace UnityEssentials
 		/// </summary>
 		/// <param name="weights">A weighted array. Higher values have a greater chance of being picked.</param>
 		/// <returns>The picked item's index.</returns>
-		public static int PickRandomWeighted(float[] weights)
+		public static int PickRandomWeighted(IList<float> weights)
 		{
 			float total = 0;
 			foreach(var w in weights) total += w;
 			float pick = Random.value * total;
-			for(int i = 0; i < weights.Length; i++)
+			for(int i = 0; i < weights.Count; i++)
 			{
 				pick -= weights[i];
 				if(pick <= 0)
@@ -169,33 +147,18 @@ namespace UnityEssentials
 					return i;
 				}
 			}
-			return weights.Length - 1;
+			return weights.Count - 1;
 		}
 
 		/// <summary>
-		/// Returns a shuffled version of the given array.
+		/// Randomly shuffles the given array.
 		/// </summary>
-		public static T[] Shuffle<T>(T[] array)
+		public static void Shuffle1<T>(IList<T> list)
 		{
-			T[] shuffled = new T[array.Length];
-			var list = array.ToList();
-			for(int i = 0; i < array.Length; i++)
+			for (int i = list.Count - 1; i > 0; i--)
 			{
-				shuffled[i] = TakeRandomItem(list);
-			}
-			return shuffled;
-		}
-
-		/// <summary>
-		/// Randomly shuffles the given list.
-		/// </summary>
-		public static void Shuffle<T>(List<T> list)
-		{
-			var elements = new List<T>(list);
-			list.Clear();
-			for(int i = 0; i < elements.Count; i++)
-			{
-				list.Add(TakeRandomItem(elements));
+				int j = Random.Range(0, i + 1);
+				(list[i], list[j]) = (list[j], list[i]);
 			}
 		}
 	}
